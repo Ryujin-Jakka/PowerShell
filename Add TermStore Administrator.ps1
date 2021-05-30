22ce9625791b1323d644b6cd122a6eb5633a896b
Ryujin Jakka <xbalrogx@hotmail.it>
2021-05-30 20:41:58 +0200

    Add files via upload

---
+++ Add TermStore Administrator.ps1
@@ -0,0 +1,22 @@
+﻿Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+$taxonomySession = Get-SPTaxonomySession -Site "SITE URL"
+$termStore = $taxonomySession.TermStores["Managed Metadata Service Application Proxy"]
+$admins = $termStore.TermStoreAdministrators | Where-Object {$_.PrincipalName -contains $env:USERNAME}
+if($admins -eq $null){
+	$termStore.AddTermStoreAdministrator($env:USERNAME)
+	$termStore.CommitAll()
+}
+
+function AddAdminToTermStore($webAppUrl, $mmsProxyName){
+	$taxonomySession = Get-SPTaxonomySession -Site $webAppUrl, $mmsProxyName
+	$termStore = $taxonomySession.TermStores[$mmsProxyName]
+	$userName = $env:USERNAME
+	$admins = $termStore.TermStoreAdministrators | Where-Object {$_.PrincipalName -contains $userName}
+	if($admins -eq $null){
+		$termStore.AddTermStoreAdministrator($env:USERNAME)
+		$termStore.CommitAll()
+	}
+	else{
+		Write-Host "The user $userName is already admin of the term store." -F Yellow
+	}
+}
